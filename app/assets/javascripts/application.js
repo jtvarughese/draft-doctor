a5932ff46c39e6c03e79f99868ad62d8ca8f854a
Joel Varughese <jtvarughese@gmail.com>
2017-05-26 15:32:41 -0400

    player db info showing; api shows but not functional

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,40 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+// Global variables
+var player;
+// variables for biometrics
+var biometric = {
+  var age = 'age'
+  var height = 'height'
+  var weight = 'weight'
+}
+// variables for History
+var history = {
+  var injuries = 'injuries'
+  var minutesPlayed = 'minutes_played'
+  var strengthOfSchedule = 'strength_of_schedule'
+}
+
+// variables for Spectrum
+var spectrum = {
+  var positionInjury = 'position_injury'
+  var severityOfInjury = 'severity_of_injury'
+}
+
+function biometric (){
+  (var age * 0.625) + (var height * 0.5) + (var weight * 0.625)
+}
+
+function history (){
+  (var injuries * 1.25) + (var minutesPlayed * 0.1) + (var strengthOfSchedule / 0.625)
+}
+
+function spectrum (){
+  (var positionInjury * 0.625) + (var severityOfInjury * 1)
+}
+
+function injuryRisk (){
+
+}
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -22,6 +22,7 @@ class PlayersController < ApplicationController
   end
 
   def show
+    @player = Player.find(params[:id])
   end
 
   def destroy
